ed7c648bec08753dcb407c8aad0030901f2456f9
Kevin Peery <kevin.peery@gmail.com>
2018-11-14 17:20:41 -0700

    controllers, models and views working

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,15 +3,41 @@ class ItemsController < ApplicationController
   before_action :set_item, except: [:index, :new, :create]
 
   def index
+    @items = @department.items
   end
 
   def show
   end
 
   def new
+    @item = @department.items.new
+    render partial: "form"
+  end
+
+  def create
+    @item = @department.items.new(item_params)
+    if @item.save
+      redirect_to department_items_path
+    else
+      render :new
+    end
   end
 
   def edit
+    render partial: "form"
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to department_items_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to department_items_path
   end
 
   private
